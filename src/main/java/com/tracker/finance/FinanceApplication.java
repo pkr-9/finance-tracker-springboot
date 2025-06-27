15fade3ee12ac9592b4278751798acbb076b37b6
Pappu Kumar <pappukumarme9999@gmail.com>
2025-06-27 09:45:33 +0530

    default package issue 1

--- src/main/java/com/tracker/finance/FinanceApplication.java
+++ src/main/java/com/tracker/finance/FinanceApplication.java
@@ -1,13 +1,11 @@
 package com.tracker.finance;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.ComponentScan;
 
-// @SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
-@SpringBootApplication(scanBasePackages = "com.tracker.finance")
-@ComponentScan(basePackages = "com.tracker.finance")
+// @SpringBootApplication(scanBasePackages = "com.tracker.finance")
+// @ComponentScan(basePackages = "com.tracker.finance")
 public class FinanceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinanceApplication.class, args);
